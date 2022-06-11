b9829a6e5a75b96d7e0a3e6b718f8c7ec13a8031
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-06-11 05:11:24 -0500

    reformatted code to be more dynamic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,65 +2,77 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
-function getUserNumberInput(){
-    return parseInt(userInput.value);
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 //generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
-    calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-    outputResult(currentResult, calcDescription); //from vendor file
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); //from vendor file
 }
 
 function writeToLog(
-    operationIdentifier, 
-    prevResult, 
-    operationNumber, 
-    newResult){
-    const logEntry = {
-        operation: operationIdentifier,
-        prevResult: prevResult,
-        number: operationNumber,
-        result: newResult
-    };
-    logEntries.push(logEntry);
-    console.log(logEntries);
-
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
 }
 
-function add (){
-    //if only working with global variables you can change value of the global variable directly 
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
+function calculateResult(calculationType) {
+  if (
+    calculationType !== 'ADD' &&
+    calculationType !== 'SUBTRACT' &&
+    calculationType !== 'MULTIPLY' &&
+    calculationType !== 'DIVIDE'
+  ) {
+    return;
+  }
+
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  if (calculationType === 'ADD') {
     currentResult += enteredNumber;
-    createAndWriteOutput("+", initialResult, enteredNumber );
-    writeToLog("ADD", initialResult, enteredNumber, currentResult);
-
-    
-    
-}
-
-function subtract(){
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
+    mathOperator = '+';
+  } else if (calculationType === 'SUBTRACT') {
     currentResult -= enteredNumber;
-    createAndWriteOutput("-", initialResult, enteredNumber );  
-    writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);  
-}
-
-function multiply(){
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
+    mathOperator = '-';
+  } else if (calculationType === 'MULTIPLY') {
     currentResult *= enteredNumber;
-    createAndWriteOutput("*", initialResult, enteredNumber );
-    writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+    mathOperator = '*';
+  } else if (calculationType === 'DIVIDE') {
+    currentResult /= enteredNumber;
+    mathOperator = '/';
+  }
+
+  createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
 }
 
-function divide(){
-    const enteredNumber = getUserNumberInput();
-    const initialResult = currentResult;
-    currentResult /= enteredNumber;
-    createAndWriteOutput("/", initialResult, enteredNumber );
-    writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+function add() {
+  //if only working with global variables you can change value of the global variable directly
+  calculateResult('ADD');
+}
+
+function subtract() {
+  calculateResult('SUBTRACT');
+}
+
+function multiply() {
+  calculateResult('MULTIPLY');
+}
+
+function divide() {
+  calculateResult('DIVIDE');
 }
 
 addBtn.addEventListener('click', add);
@@ -72,10 +84,16 @@ divideBtn.addEventListener('click', divide);
 /*What I learned
 Learned operators, variables, and constants
 learned about arrays/objects and functions
+learned arrays behind the scenes are objects
 learned about how to pass data into functions
 learnd syntax
 learned about optimizing scripts for shipping best code to users (async/defer)
+learned to optimize code by reducing repeatable code (DRY) and reducing hard
+coded values making code more dynamic
+learned calling funtions inside of other function
+learned if you set an object/array equal to another object they will be equal because
+they point to same place in memory
+learned if you create two objects/arrays of same type with identical information
+they will NOT be equal because they point to seperate created instances in memory 
+learned to reduce nested if statements
 */
-
-
-
